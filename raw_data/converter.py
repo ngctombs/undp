5941a6e582c76f1e2d04dcfb8e0cda139e20482b
Nathanael Tombs <ngctombs@gmail.com>
2013-08-17 01:40:52 -0700

    Cleaned data set, producing a json file per indicator (beware of next commit)

--- raw_data/converter.py
+++ raw_data/converter.py
@@ -40,9 +40,35 @@ def XMLToJson (inp, outp) :
 	xmlOutput.write(json.dumps(xmltodict.parse( xmlInput )))
 	xmlOutput.close()
 
-csvToJson('units.csv', 'units.json')
-csvToJson('countries.csv', 'countries.json')
-XMLToJson('un_hdi.xml', 'un_hdi.json')
-XMLToJson('data2012.xml', 'data.json')
 
 print('Victory is mine!')
+
+dataInput2012 = open( 'data2012.xml' )
+data = xmltodict.parse( dataInput2012 )
+countriesInput = open( 'countries.csv' )
+countries = [ x for x in csv.DictReader( countriesInput ) ]
+indicatorInput = open( 'un_hdi.xml' )
+indicators = xmltodict.parse( indicatorInput )
+
+print(data['DocumentElement']['Table1'][0]['country'])
+#STEP 1 : link UN Country codes to their data sets (tuples)
+countrySet = {data['DocumentElement']['Table1'][z]['country'] : [(x, data['DocumentElement']['Table1'][z][x]) for x in data['DocumentElement']['Table1'][z] if data['DocumentElement']['Table1'][z][x] != None and x[0:4] == 'indi'] for z in range(len(data['DocumentElement']['Table1']))}
+
+#STEP 2 : replace UN Country codes by ISO-2 codes (eg. CA, US, etc.)
+isoSet = {countries[z]['iso2']:countrySet[countries[z]['country']] for z in range(len(countries)) if countries[z]['country'] in countrySet }
+
+#STEP 3 : change from country:[(indicator, value), ..] to indicator:[(country, value), ...]
+indicatorDict = dict()
+for x in isoSet:
+	for y in isoSet[x]:
+		if y[0] in indicatorDict :
+			indicatorDict[y[0]].append((x, y[1]))
+		else :
+			indicatorDict[y[0]] = [(x, y[1])]
+
+#STEP 4 : write clean sets to files
+for x in indicatorDict.keys():
+	#indicatorFile = open( x + '.json', 'w' )
+	y = json.dumps( indicatorDict )
+	#indicatorFile.close()
+print(y)
